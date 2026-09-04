14cf90837ea6d46175985c5b4874bcd50006bd48
chen <changtaichen017@gmail.com>
2026-09-04 22:43:25 +0900

    exp: validation experiments

--- kaggle/run.py
+++ kaggle/run.py
@@ -61,11 +61,8 @@ PROJECT_DIR = WORK_DIR / "rxrx1"
 GIT_REF = "experiment/normalization"
 
 CONFIGS = [
-    "configs/normalization/efficientnet_b2_norm/pixel/effb2_norm_src-ref_pop-np_grp-global_sp-pixel_ch-shared_pol-trainonly_pos-pre.yaml",
-"configs/normalization/efficientnet_b2_norm/pixel/effb2_norm_src-ref_pop-np_grp-exp_sp-pixel_ch-shared_pol-trainonly_pos-pre.yaml",
-"configs/normalization/efficientnet_b2_norm/pixel/effb2_norm_src-ref_pop-np_grp-plate_sp-pixel_ch-shared_pol-trainonly_pos-pre.yaml",
+    "configs/normalization/efficientnet_b2_norm/val/effb2_norm_src-ref_pop-np_grp-plate_sp-global_ch-shared_pol-all_pos-pre.yaml"
 ]
-
 # =============================================================================
 
 
